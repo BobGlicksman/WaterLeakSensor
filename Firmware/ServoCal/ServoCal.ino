1ef7a964494896819b0161d1f3314ba19949cd5e
Jim Schrempp <github1@jimschrempp.com>
2017-03-16 11:03:18 -0700

    moved ServoCal to new folder. Changed return code so that actual servo position is returned if a limit is exceeded

--- Firmware/ServoCal/ServoCal.ino
+++ Firmware/ServoCal/ServoCal.ino
@@ -2,16 +2,16 @@
     The servo is connected to a 3.3 volt to 5 volt converter.  The converter
     input is connected to Photon pin D3.  A 5.6K ohm pulldown resistor is used
     to ensure that the servo is quiet while the Photon is being flashed/reset.
-    
+
     The SIS debug client is used to specify positions for the servo.  In order
     to calibrate the servo, the servo with a pointer is mounted on to the meter
-    scale.  The user specifies positions for the sero and observes where the 
+    scale.  The user specifies positions for the sero and observes where the
     servo pointer is on the meter scale.  The position values for the scale minimum
     and for the scale maximum are determined by trial and error and the final
     values are noted for use in the WaterLeakDetector program.
-    
+
     date: 3/07/17, by Bob Glicksman
-    
+
     (c) 2017 by Bob Glicksman, Jim Schrempp, and team Practical Projects
 */
 
@@ -24,19 +24,19 @@ int pos = (MAX_POS - MIN_POS)/2;    // global variable to store the servo positi
 
 void setup() {
     Particle.function("Servo", servoCmd);
-    
+
     myservo.attach(A5);  // attaches pin A5 to the servo object
     delay(2000);  // wait 2 seconds before continuing
 
 } // end of setup
 
 void loop() {
-    myservo.write(pos); 
+    myservo.write(pos);
 } // end of loop
 
 int servoCmd(String cmd) {
     int servoPosition = cmd.toInt(); // get the user's desired position for the servo
-    
+
     if(servoPosition > MAX_POS)  {
         pos = MAX_POS;
     }
@@ -46,6 +46,6 @@ int servoCmd(String cmd) {
     else  {
         pos = servoPosition;
     }
-    
-    return servoPosition;
+
+    return pos;
 }  // end of servoCommand
